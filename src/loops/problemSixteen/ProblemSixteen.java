908724059ed11a17ec03cf3183fef3c40a1944f0
“hussainmahbub88” <hussainmahbub88@gmail.com>
2018-10-24 13:56:22 -0400

    problem sixteen updated

--- src/loops/problemSixteen/ProblemSixteen.java
+++ src/loops/problemSixteen/ProblemSixteen.java
@@ -1,28 +1,14 @@
 package loops.problemSixteen;
+
 public class ProblemSixteen {
 
-//	static int countMultiples(int n) {
-//		int result = 0;
-//		for (int i = 1; i <= n; i++) {
-//			if (i % 3 == 0 || i % 7 == 0) {
-//				result++;
-//			}
-//			
-//		}
-//		return result;
-//	}
-//	public static void main(String[] args) {
-//		
-//		System.out.print("Count = "); 
-//	    System.out.println(countMultiples(25)); 
-//
-//	}
-
 	public static void main(String[] args) {
-		
+
 		for (int i = 21; i <= 1944; i++) {
 			if (i % 3 == 0 || i % 7 == 0) {
 				System.out.println(i);
+			}else if (i % 3 == 0 && i % 7 == 0) {
+				System.out.println();
 			}
 		}
 	}
