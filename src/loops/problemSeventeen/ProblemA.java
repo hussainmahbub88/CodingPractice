8bd2c43f0a2b7962a39b778f46c7562232901734
“hussainmahbub88” <hussainmahbub88@gmail.com>
2018-10-24 16:53:06 -0400

    problem Seventeen done

---
+++ src/loops/problemSeventeen/ProblemA.java
@@ -0,0 +1,18 @@
+package loops.problemSeventeen;
+
+public class ProblemA {
+
+	public static void main(String[] args) {
+		int n1 = 81, n2 = 153;
+
+		while (n1 != n2) {
+			if (n1 > n2)
+				n1 -= n2;
+			else
+				n2 -= n1;
+		}
+
+		System.out.println("Greatest Common Divisor of two integers are : " + n1);
+	}
+
+}
