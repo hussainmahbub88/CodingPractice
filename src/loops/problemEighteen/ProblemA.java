c993cc3df2db28d8322df231c34a6fe2d23d1ef2
“hussainmahbub88” <hussainmahbub88@gmail.com>
2018-10-24 17:03:08 -0400

    problem eighteen done

---
+++ src/loops/problemEighteen/ProblemA.java
@@ -0,0 +1,25 @@
+package loops.problemEighteen;
+
+import java.util.Scanner;
+
+public class ProblemA {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the number :");
+		int number = sc.nextInt();
+        long fact = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+		
+        /*
+         * Formula to find factorial is 
+         * n! = n * (n-1) * (n-2)*....*1
+         */
+
+	}
+
+}
--- src/loops/problemSeventeen/ProblemB.java
+++ src/loops/problemSeventeen/ProblemB.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class ProblemB {
 
 	public static void main(String[] args) {
-		int num = 0;
-		int reversenum = 0;
-		
+		int num = 0, reversenum = 0;
+
 		System.out.println("Input your number and press enter: ");
 		Scanner in = new Scanner(System.in);
 		num = in.nextInt();
-		
+
 		while (num != 0) {
 			reversenum = reversenum * 10;
 			reversenum = reversenum + num % 10;
