c1a4592f1646d548ac9fd7125a2c139ad5184c83
“hussainmahbub88” <hussainmahbub88@gmail.com>
2018-10-24 12:03:45 -0400

    problem fifteen done

---
+++ src/loops/problemFifteen/ProblemFifteen.java
@@ -0,0 +1,14 @@
+package loops.problemFifteen;
+
+public class ProblemFifteen {
+
+	public static void main(String[] args) {
+		for (int i = 13; i <= 999; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
